e6e634823557c60a7e56717dc5643c1c234efc1e
hgutierrez <hgutierrez@OSC0100239>
2022-01-05 16:15:21 -0400

    gifsApp 2edt

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,15 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GifsService {
-
+private apiKey : string = 'wvNY1hte3cNF31Ii45OLWDkjsRukxqZn';
  private _historial : string[] = [];
 
  get historial(){
    return [...this._historial];
  }
 
- buscarGifs( query: string){
+ buscarGifs( query: string = ''){
+
+query = query.trim().toLowerCase();
+
+  if(!this._historial.includes( query)){
     this._historial.unshift( query );
-    console.log(this._historial)
+    this._historial = this._historial.splice(0,10);
+  }
+
+
  }
 }
+
