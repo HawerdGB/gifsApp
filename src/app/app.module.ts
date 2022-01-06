54c76114652794185d587342cc4d839c650007c8
hgutierrez <hgutierrez@OSC0100239>
2022-01-06 10:57:27 -0400

    uso de httpClient y localStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     GifsModule
   ],
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGIFResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +9,20 @@ export class GifsService {
 private apiKey : string = 'wvNY1hte3cNF31Ii45OLWDkjsRukxqZn';
  private _historial : string[] = [];
 
+ public resultados: Gif[] = [];
+
  get historial(){
    return [...this._historial];
+ }
+
+ constructor (private http: HttpClient){
+   if(localStorage.getItem('historial')){
+      this._historial = JSON.parse(localStorage.getItem('historial')!);
+       }
+   localStorage.getItem('historial');
+//esta es otra manera mas sencilla de hacer los mismo de arriba , en una sola linea
+this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+
  }
 
  buscarGifs( query: string = ''){
@@ -18,8 +32,27 @@ query = query.trim().toLowerCase();
   if(!this._historial.includes( query)){
     this._historial.unshift( query );
     this._historial = this._historial.splice(0,10);
+
+    localStorage.setItem('historial',JSON.stringify(this._historial) );
   }
 
+  //este metodo que nos proporciona angular a traves del HttpClient, usando
+  this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=wvNY1hte3cNF31Ii45OLWDkjsRukxqZn&q=${ query }&limit=10`)
+  .subscribe( resp => {
+   // console.log(resp.data);
+    this.resultados = resp.data;
+    localStorage.setItem('resultados',JSON.stringify(this.resultados));
+
+  })
+
+//fetch('https://api.giphy.com/v1/gifs/search?api_key=wvNY1hte3cNF31Ii45OLWDkjsRukxqZn&q=dbz&limit=10')
+//.then( resp => {resp.json().then(data => console.log(data))})
+
+//otra forma es poniendole async a buscarGifts y creando las const para la resp., daria el mismo resultado
+
+//const resp =  await fetch('https://api.giphy.com/v1/gifs/search?api_key=wvNY1hte3cNF31Ii45OLWDkjsRukxqZn&q=dbz&limit=10');
+//const data = await resp.json();
+//console.log(data);
 
  }
 }
